65836b6a6d3e50e0c8d631ab86d2cfbb95d06700
livc <95livc@gmail.com>
2017-11-02 13:01:22 +0800

    test email

--- media/js/center-images.js
+++ media/js/center-images.js
@@ -7,16 +7,3 @@ window.onload = function() {
       img.parentElement.style.textAlign = 'center';
   }
 };
-
-# 邮箱地址复制反转
-<script type="text/javascript">
-var reversedEmail = document.getElementById("email").innerText;
-document.getElementById("email").addEventListener('copy', function(e) {
-    var selection = window.getSelection().toString();
-    if (selection.indexOf(reversedEmail) >= 0) {
-        var normalEmail = selection.split("").reverse().join("");
-        e.clipboardData.setData("text/plain", normalEmail);
-    }
-    e.preventDefault();
-});
-</script>
